e23908e67740ea2faa677f36e6fe6e2f32ca1933
Krishna Kumar <krishnadevbit@gmail.com>
2024-08-15 08:43:18 +0530

    making error free for cart line items undefined

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -84,58 +84,63 @@ const CartModal = ({ handleCartOpen }: CartModalProps) => {
           {/* LIST */}
           <div className="flex flex-col gap-8">
             {/* ITEM */}
-            {cart.lineItems.map((item) => (
-              <div className="flex gap-4" key={item._id}>
-                {item.image && (
-                  <Image
-                    src={wixMedia.getScaledToFillImageUrl(
-                      item.image,
-                      72,
-                      96,
-                      {}
-                    )}
-                    alt=""
-                    width={72}
-                    height={96}
-                    className="object-cover rounded-md"
-                  />
-                )}
-                <div className="flex flex-col justify-between w-full">
-                  {/* TOP */}
-                  <div>
-                    {/* TITLE */}
-                    <div className="flex items-center justify-between gap-8">
-                      <h3 className="font-semibold">
-                        {item.productName?.original}
-                      </h3>
-                      <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
-                        {item.quantity && item.quantity > 1 && (
-                          <div className="text-xs text-green-500">
-                            {item.quantity} x{" "}
-                          </div>
-                        )}
-                        ${item.price?.amount}
+            {cart.lineItems &&
+              cart.lineItems.map((item) => (
+                <div className="flex gap-4" key={item._id}>
+                  {item.image && (
+                    <Image
+                      src={wixMedia.getScaledToFillImageUrl(
+                        item.image,
+                        72,
+                        96,
+                        {}
+                      )}
+                      alt=""
+                      width={72}
+                      height={96}
+                      className="object-cover rounded-md"
+                    />
+                  )}
+                  <div className="flex flex-col justify-between w-full">
+                    {/* TOP */}
+                    <div>
+                      {/* TITLE */}
+                      <div className="flex items-center justify-between gap-8">
+                        <h3 className="font-semibold">
+                          {item.productName?.original}
+                        </h3>
+                        <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
+                          {item.quantity && item.quantity > 1 && (
+                            <div className="text-xs text-green-500">
+                              {item.quantity} x{" "}
+                            </div>
+                          )}
+                          ${item.price?.amount}
+                        </div>
+                      </div>
+                      {/* DESC */}
+                      <div className="text-sm text-gray-500">
+                        {item.availability?.status}
                       </div>
                     </div>
-                    {/* DESC */}
-                    <div className="text-sm text-gray-500">
-                      {item.availability?.status}
+                    {/* BOTTOM */}
+                    <div className="flex justify-between text-sm">
+                      <span className="text-gray-500">
+                        Qty. {item.quantity}
+                      </span>
+                      <span
+                        className="text-blue-500"
+                        style={{
+                          cursor: isLoading ? "not-allowed" : "pointer",
+                        }}
+                        onClick={() => removeItem(wixClient, item._id!)}
+                      >
+                        Remove
+                      </span>
                     </div>
                   </div>
-                  {/* BOTTOM */}
-                  <div className="flex justify-between text-sm">
-                    <span className="text-gray-500">Qty. {item.quantity}</span>
-                    <span
-                      className="text-blue-500"
-                      style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
-                      onClick={() => removeItem(wixClient, item._id!)}
-                    >
-                      Remove
-                    </span>
-                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
           {/* BOTTOM */}
           <div>
