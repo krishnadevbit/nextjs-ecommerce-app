0a3dfe3e7fd76369ca36a25bafdaf010025d5dcd
Krishna Kumar <krishnadevbit@gmail.com>
2024-08-15 21:26:04 +0530

    make server comp as server component

---
+++ src/components/SuccessComp.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Skeleton from "@/components/Skeleton";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import Confetti from "react-confetti";
+
+const SuccessPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const orderId = searchParams.get("orderId");
+  console.log("orderId", orderId);
+
+  useEffect(() => {
+    if (!orderId) return;
+
+    const timer = setTimeout(() => {
+      router.push(`/orders?orderId=${orderId}`);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [orderId, router]);
+
+  return (
+    <Suspense fallback={<Skeleton />}>
+      <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
+        <Confetti width={2000} height={1000} />
+        <h1 className="text-5xl text-green-700">
+          Order Successfully Placed 🎉
+        </h1>
+        <h2 className="text-xl font-medium">
+          We sent the invoice to your e-mail
+        </h2>
+        <h3 className="">You are being redirected to the order page...</h3>
+      </div>
+    </Suspense>
+  );
+};
+
+export default SuccessPage;
